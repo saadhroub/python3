6eaf8265733a7bb07438e3e0086b72de9ced0331
Saad Hroub <saad.hroub@gmail.com>
2023-08-02 12:32:59 +0300

    function with return

---
+++ day3/school.py
@@ -0,0 +1,14 @@
+def school(g1,g2,g3):
+    sum = g1+g2+g3
+    avg = int(sum/3)
+    return avg
+    
+
+avg1 = school(90,88,71)
+avg2 = school(88,60,71)
+avg3 = school(66,56,71)
+print(avg1)
+print(avg2)
+print(avg3)
+
+#check_avgs(avg1,avg3,avg2)
