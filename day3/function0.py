707675cb9fbca3e85df7ddec570a637a3ecff5c9
Saad Hroub <saad.hroub@gmail.com>
2023-08-02 11:50:20 +0300

    my first function

---
+++ day3/function0.py
@@ -0,0 +1,12 @@
+def greeting(name, city):
+    print("Welcome our dear student")
+    print("Your name is: ",name)
+    print("You are from: ", city)
+    print("You are studing Python in Amideast")
+    print("This is level 1 of Python")
+    print("****************************")
+
+greeting("Ahmad", "Tulkarem")
+greeting("Noor", "Ramallah")
+greeting("Kamil", "Nazareth")
+
