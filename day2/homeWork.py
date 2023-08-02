4a8011098330b1df85b06176a54b80c7f0489888
Saad Hroub <saad.hroub@gmail.com>
2023-08-02 11:13:26 +0300

    adding KAMIL work

--- day2/homeWork.py
+++ day2/homeWork.py
@@ -1,28 +1,28 @@
-# import math
-# print("let's start with Q1:-")
-# x = "********************************************************"
-# first_name = input("Enter your first name: ")
-# father_name = input("Enter your father name: ")
-# grand_father_name  = input("Enter grandfather name: ")
-# family_name = input("Enter Family name: ")
+import math
+print("let's start with Q1:-")
+x = "********************************************************"
+first_name = input("Enter your first name: ")
+father_name = input("Enter your father name: ")
+grand_father_name  = input("Enter grandfather name: ")
+family_name = input("Enter Family name: ")
 
-# print(first_name.upper(), father_name.upper(), grand_father_name.upper(), 
-#     family_name.upper())
-# print(x)
-# ###############################################################
+print(first_name.upper(), father_name.upper(), grand_father_name.upper(), 
+    family_name.upper())
+print(x)
+###############################################################
 
-# #Question 2:Write a python code that asks the user to insert the student final marks
-# # for Arabic, English, Science, and Math. Then calculate the average of the student
+#Question 2:Write a python code that asks the user to insert the student final marks
+# for Arabic, English, Science, and Math. Then calculate the average of the student
 
-# Math = int(input("Please insert Math marks:  "))
-# English = int(input("Please insert English marks:  "))
-# Science  = int(input("Please insert Science marks:  "))
-# Arabic = int(input("Please insert Arabic marks:  "))
+Math = int(input("Please insert Math marks:  "))
+English = int(input("Please insert English marks:  "))
+Science  = int(input("Please insert Science marks:  "))
+Arabic = int(input("Please insert Arabic marks:  "))
 
-# sum = Math + English + Science + Arabic
-# print(int(sum/4))
+sum = Math + English + Science + Arabic
+print(int(sum/4))
 
-# #################################################################
+#################################################################
 
 sta = input("Enter long text: ")
 
