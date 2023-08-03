12bcafe9b84a2325f07bdef760beae2b4d46c08d
Saad Hroub <saad.hroub@gmail.com>
2023-08-03 12:33:59 +0300

    calling functions by if statements

--- day4/conditions.py
+++ day4/conditions.py
@@ -2,14 +2,12 @@ sunny = True
 passport = True
 money = int(input("Enter the money: "))
 
-if sunny and passport:
-    if money>=5000:
-        print("We will go Maldiv")
-    elif money>=2000:
-        print("We will go Antalya")
-    else:
-        print("We will go Aqaba")
-else:
+if sunny and passport and money>=5000:
+    print("We will go Maldiv")
+elif sunny and passport and money>=2000:
+    print("We will go Turkey")
+else: 
     print("Home Sweet Home")
+    
 
         
