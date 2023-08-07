4c95f344ff48c290de1cd10e4925a150879a0f30
Saad Hroub <saad.hroub@gmail.com>
2023-08-07 10:39:46 +0300

    list examples

---
+++ day6/list1.py
@@ -0,0 +1,12 @@
+grades = [98, 88, 78, 91, 90, 70,10,11]
+
+print("***************", len(grades))
+
+
+for i in range(0, 5):
+    grades.append(input("Enter the new Element" ))
+
+for i in range(0, len(grades)):
+    print(grades[i])
+
+
